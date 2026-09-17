94120938d0f4d709cdd2ecf79776b333741a2762
kferrano <rzimpel6@gmail.com>
2026-09-17 14:19:57 +0200

    Replace `ModScanResponsePacket` with `ModScanChunkPacket` in `Net` setup and update protocol version to 2

--- src/main/java/com/hardrock/modwhitelist/network/Net.java
+++ src/main/java/com/hardrock/modwhitelist/network/Net.java
@@ -1,8 +1,8 @@
 package com.hardrock.modwhitelist.network;
 
 import com.hardrock.modwhitelist.Modwhitelist;
+import com.hardrock.modwhitelist.network.packet.ModScanChunkPacket;
 import com.hardrock.modwhitelist.network.packet.ModScanRequestPacket;
-import com.hardrock.modwhitelist.network.packet.ModScanResponsePacket;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.api.distmarker.Dist;
@@ -12,7 +12,7 @@ import net.minecraftforge.network.PacketDistributor;
 import net.minecraftforge.network.simple.SimpleChannel;
 
 public final class Net {
-    private static final String PROTOCOL = "1";
+    private static final String PROTOCOL = "2";
     private static int id = 0;
 
     public static final SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(
@@ -35,10 +35,10 @@ public final class Net {
 
         CHANNEL.registerMessage(
                 id++,
-                ModScanResponsePacket.class,
-                ModScanResponsePacket::encode,
-                ModScanResponsePacket::decode,
-                ModScanResponsePacket::handle
+                ModScanChunkPacket.class,
+                ModScanChunkPacket::encode,
+                ModScanChunkPacket::decode,
+                ModScanChunkPacket::handle
         );
 
         DistExecutor.safeRunWhenOn(Dist.CLIENT,
