417b0159afb5b1f432ccc8cc44a6606caffeb8df
kferrano <rzimpel6@gmail.com>
2026-09-20 11:22:14 +0200

    Update network protocol to v2 and replace ModScanResponsePacket with ModScanChunkPacket

--- src/main/java/com/hardrock/modwhitelist/network/Net.java
+++ src/main/java/com/hardrock/modwhitelist/network/Net.java
@@ -1,8 +1,8 @@
 package com.hardrock.modwhitelist.network;
 
 import com.hardrock.modwhitelist.Modwhitelist;
+import com.hardrock.modwhitelist.network.packet.ModScanChunkPacket;
 import com.hardrock.modwhitelist.network.packet.ModScanRequestPacket;
-import com.hardrock.modwhitelist.network.packet.ModScanResponsePacket;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.api.distmarker.Dist;
@@ -12,11 +12,11 @@ import net.minecraftforge.network.PacketDistributor;
 import net.minecraftforge.network.simple.SimpleChannel;
 
 public final class Net {
-    private static final String PROTOCOL = "1";
+    private static final String PROTOCOL = "2";
     private static int id = 0;
 
     public static final SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(
-            ResourceLocation.fromNamespaceAndPath(Modwhitelist.MODID, "main"),
+            new ResourceLocation(Modwhitelist.MODID, "main"),
             () -> PROTOCOL,
             PROTOCOL::equals,
             PROTOCOL::equals
@@ -35,13 +35,14 @@ public final class Net {
 
         CHANNEL.registerMessage(
                 id++,
-                ModScanResponsePacket.class,
-                ModScanResponsePacket::encode,
-                ModScanResponsePacket::decode,
-                ModScanResponsePacket::handle
+                ModScanChunkPacket.class,
+                ModScanChunkPacket::encode,
+                ModScanChunkPacket::decode,
+                ModScanChunkPacket::handle
         );
 
-        DistExecutor.safeRunWhenOn(Dist.CLIENT,
+        DistExecutor.safeRunWhenOn(
+                Dist.CLIENT,
                 () -> com.hardrock.modwhitelist.network.client.ClientHandlers::init
         );
     }
